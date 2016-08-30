158f74797ad2487e6327bc78565d1c5d74677154
Zhijing Xue <Zhijing Xue>
2016-08-30 14:41:59 +0800

    Signed-off-by: Zhijing Xue <Zhijing Xue>

---
+++ server/index.js
@@ -0,0 +1,23 @@
+'use strict';
+const app = require('express')();
+const bodyParser = require('body-parser');
+app.use(bodyParser());
+
+const notify_url = 'http://wxtest.tlifang.com/wxqrcodepay';
+const redirect_uri = 'http://wxtest.tlifang.com/redirect';
+
+const WX = require('ym-modules').wx;
+const wx = new WX(require('./WXCONFIG.json'));
+
+app.get('openid_url', function(req, res) {
+  let uri = wx.getOpenId(redirect_uri);
+  res.send(uri);
+});
+
+app.get('/redirect', function(req, res) {
+  console.log('query', req.query);
+  console.log('body', req.body);
+  res.end('heheda');
+});
+
+app.listen('')
